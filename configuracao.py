7317a3d43fd9a5dcc9a7bf9ffd954dc8bbdbd565
Guilherme Abreu <guilhermeabreeu6@gmail.com>
2025-11-05 12:09:22 -0300

    Update initialization message in configuracao.py

--- configuracao.py
+++ configuracao.py
@@ -21,7 +21,7 @@ class ConfiguracaoSistema:
         (ex: leitura de arquivo) só ocorra uma vez.
         """
         if not hasattr(self, '_inicializado'):
-            print("ConfiguracaoSistema: Carregando configurações iniciais...")
+            print("Logicargo System: Carregando configurações iniciais...")
 
             # Parâmetros de configuração
             self.TAXA_KM_VAN = 0.50
